e6949162144f39a5b83c6851cdd3cb130ba73cf7
remco-stam <remcostam@hotmail.com>
2014-11-20 11:13:51 +0000

    Update Create_subset.py

--- motifs/Create_subset.py
+++ motifs/Create_subset.py
@@ -1,4 +1,7 @@
 ##The aim of this script is to extract seq from a fasta file and create a new fasta file
+##Script is based on a script by Gaetan Thilliez (PS, Uni of Dundee)
+##Amended to take a table with names and cluster number and extract sequences from a fasta file with upstream sequences
+
 
 
 def ExtractFastaSeq (List, Fasta):
